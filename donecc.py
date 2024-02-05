4bfe464d8e6c8b5e460c369b105462bab7b8a462
Shivam-Netfotech <155534371+Shivam-Netfotech@users.noreply.github.com>
2024-02-05 10:24:17 +0530

    Add files via upload

---
+++ donecc.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, HTTPException
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from PyPDF2 import PdfReader
+import os
+
+app = FastAPI()
+
+os.environ["OPENAI_API_KEY"] = "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"
+embeddings = OpenAIEmbeddings()
+
+pdfreader = PdfReader("/Users/rakesh/PycharmProjects/python_ChatUI/09. Understanding Anxiety in Children and Teens Author Child Mind Institute.pdf")
+
+raw_text = ''
+for i, page in enumerate(pdfreader.pages):
+    content = page.extract_text()
+    if content:
+        raw_text += content
+
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=800,
+    chunk_overlap=200,
+    length_function=len,
+)
+texts = text_splitter.split_text(raw_text)
+
+document_search = FAISS.from_texts(texts, embeddings)
+
+chain = load_qa_chain(OpenAI(), chain_type="stuff")
+
+chat_history = []
+
+@app.post("/chatbot")
+async def chatbot_endpoint(user_input: str):
+    if user_input.lower() == 'exit':
+        raise HTTPException(status_code=200, detail="Exiting chatbot.")
+
+    docs = document_search.similarity_search(user_input)
+    result = chain({"question": user_input, "chat_history": chat_history, "input_documents": docs, "temperature": 0.5})
+    response = result.get('output_text', 'No answer found')
+    chat_history.append((user_input, response))
+
+    return {"response": response}
